189afc7c89216c3d58e7f5a70c8a7a35e37b5bdf
unknown <fajardillah25@gmail.com>
2024-07-30 20:42:43 +0700

    Upload

---
+++ src/main/java/jakarta/fajar/Address.java
@@ -0,0 +1,48 @@
+package jakarta.fajar;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class Address {
+
+
+    @NotBlank(message = "Street can't blank ")
+    private String street;
+    @NotBlank(message = "city can't blank ")
+    private String city;
+    @NotBlank(message = "country can't blank ")
+    private String country;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
